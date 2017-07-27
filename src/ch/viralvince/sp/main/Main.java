5d13b9fb6d3ae29add691f6416cd957f88042fe3
ViralVince <vincent.peuker@gmail.com>
2017-07-27 19:41:00 +0200

    g

--- src/ch/viralvince/sp/main/Main.java
+++ src/ch/viralvince/sp/main/Main.java
@@ -1,26 +1,26 @@
 package ch.viralvince.sp.main;
 
-import java.util.*;
-
 public class Main {
 
 	public static void main (String[] args) {
-		System.out.println("Aretmetische Reie");
-		Scanner Eingabe = new Scanner(System.in);                                                
-		System.out.println("Zahl?"); 
-		String zahl = Eingabe.next();
-		Integer x = Integer.valueOf(zahl);
-		Integer e = 0;
-		Integer j = 0;
-		for(int i=1; i<x+1; i++) {
-			j = j+2;
-			e = e+j-1;
+//		System.out.println("Aretmetische Reie");
+//		Scanner Eingabe = new Scanner(System.in);                                                
+//		System.out.println("Zahl?"); 
+//		String zahl = Eingabe.next();
+//		Integer x = Integer.valueOf(zahl);
+//		Integer e = 0;
+//		Integer j = 0;
+//		for(int i=1; i<x+1; i++) {
+//			j = j+2;
+//			e = e+j-1;
+		int rarityRandom = (int) (Math.random() * 2);
+		System.out.println(rarityRandom);
 		}
 		
 		
-		System.out.println("Das Ergebnis ist " + e);
-		Eingabe.close();
-	}
+//		System.out.println("Das Ergebnis ist " + e);
+//		Eingabe.close();
+//	}
 	
 	
 //G
