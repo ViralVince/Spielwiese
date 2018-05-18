a2f710d509a4ced0dfb13ea5528705f36b969055
ViralVince <vincent.peuker@gmail.com>
2018-05-18 12:28:58 +0200

    c

--- src/ch/viralvince/sp/main/Primzahlen.java
+++ src/ch/viralvince/sp/main/Primzahlen.java
@@ -5,12 +5,17 @@ public class Primzahlen {
 	public boolean prüfeZahl(int i) {
 		boolean b= false;
 		int c=2;
-		while(b== false) {
+		while(b == false) {
 			if(i%c==0) {
-				
+				if(i!=c) {
+					b = true;
+					return true;
+				} else {
+					return false
+				}
 			}
 		}
-			return true;
+			
 	}
 	
 	
